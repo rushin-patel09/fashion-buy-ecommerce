f95b4a6a7464f4f67582a25d1d84f326306b2eb3
RUSHIN PATEL <204936600+rushin-patel09@users.noreply.github.com>
2025-05-04 04:55:14 +0530

    another commit V1- Solved Issue on Phone Devices NavBar not Closing

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,7 +5,7 @@ import { NavLink } from "react-router";
 import { RxHamburgerMenu, RxCross2 } from "react-icons/rx";
 import { ShopContext } from "../Context/ShopContext";
 
-const NavBarList = () => {
+const NavBarList = ({setIsOpen}) => {
   const { getTotalCartItems } = useContext(ShopContext);
   const navlinkActive = ({ isActive }) => {
     return {
@@ -16,27 +16,32 @@ const NavBarList = () => {
     };
   };
 
+  const handlerClickCategory = () => {
+    setIsOpen(false);
+  }
+
   return (
     <>
-      <NavLink to="/shop" style={navlinkActive} className="px-5">
+      <NavLink to="/shop" style={navlinkActive} className="px-5" onClick={handlerClickCategory}>
         Shop
       </NavLink>
-      <NavLink to="/men" style={navlinkActive} className="px-5">
+      <NavLink to="/men" style={navlinkActive} className="px-5" onClick={handlerClickCategory}>
         Men
       </NavLink>
-      <NavLink to="/women" style={navlinkActive} className="px-5">
+      <NavLink to="/women" style={navlinkActive} className="px-5" onClick={handlerClickCategory}>
         Women
       </NavLink>
-      <NavLink to="/kids" style={navlinkActive} className="px-5">
+      <NavLink to="/kids" style={navlinkActive} className="px-5" onClick={handlerClickCategory}>
         Kids
       </NavLink>
       <NavLink
         to="/login"
         className="px-5 py-1 rounded-2xl border-2 py-auto hover:bg-[#ffe3e0] transition-all ease-in-out"
+        onClick={handlerClickCategory}
       >
         Login
       </NavLink>
-      <NavLink to="/cart" className="px-5 relative">
+      <NavLink to="/cart" className="px-5 relative" onClick={handlerClickCategory}>
         <img src={CartLogo} alt="Cart" width={28} />
         <span className="absolute top-[-20%] right-2 bg-black text-white rounded-full min-w-[20px] max-w-max text-center font-bold text-sm px-0.5">
           {getTotalCartItems()}
@@ -61,7 +66,7 @@ const NavBar = () => {
           </NavLink>
         </div>
         <div className="flex items-center gap-2 max-[824px]:hidden">
-          <NavBarList />
+          <NavBarList setIsOpen={setIsOpen}/>
         </div>
 
         <div className="hidden items-center max-[824px]:flex">
@@ -75,7 +80,7 @@ const NavBar = () => {
           key="unique"
           className="flex flex-col items-center gap-5 pt-5 transition-opacity duration-500 opacity-0 animate-fade-in md:hidden"
         >
-          <NavBarList />
+          <NavBarList setIsOpen={setIsOpen}/>
         </div>
       )}
     </nav>
